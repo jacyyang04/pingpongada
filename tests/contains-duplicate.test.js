2d762fcf568887353e0e608fff334125b86c3eef
jacy <jacyyang04@gmail.com>
2021-12-03 11:22:03 -0800

    first commit

---
+++ tests/contains-duplicate.test.js
@@ -0,0 +1,7 @@
+import { containsDuplicate } from "../src";
+
+describe("Contains Duplicate", () => {
+  test("constainsDuplicate is defined", () => {
+    expect(containsDuplicate).toBeDefined();
+  });
+});
